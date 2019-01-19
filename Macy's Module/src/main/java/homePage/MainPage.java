bc275bcc256ab917152b34d8d73a01451bf074d2
nayeem3814 <nayeemsisir156@gmail.com>
2019-01-18 19:28:34 -0500

    Macys Commit-2

--- Macy's Module/src/main/java/homePage/MainPage.java
+++ Macy's Module/src/main/java/homePage/MainPage.java
@@ -5,64 +5,40 @@ import org.openqa.selenium.support.FindBy;
 import reporting.TestLogger;
 
 public class MainPage extends BaseUtil {
-
     @FindBy(id = "Layer_1")
-    //@FindBy (xpath ="//svg[@id='Layer_1']")
-    //@FindBy (xpath = "//*[@id="Layer_1"])  // LOGO-Passed
     public static WebElement logo;
-
     @FindBy (xpath ="//a[@title='STORES']")
-    //@FindBy(id = "STORES")                      // Passed
-    //@FindBy(xpath ="//*[@id=\"link-rail\"]/div/div/div[1]/nav/ul/li[1]/a")
     public static WebElement stores;
-
     @FindBy(xpath ="//*[@id=\"link-rail\"]/div/div/div[1]/nav/ul/li[2]/a")
-    //@FindBy(id = "DEALS")
-    //@FindBy(css = ".header-menu-dropdown")        // Passed
     public static WebElement deals;
-
     @FindBy(xpath ="//*[@id=\"header-wishlist-label\"]/a")
-    //@FindBy (id= "LISTS")                         // Passed
     public static WebElement lists;
-
-    @FindBy (id= "giftsLink")                       // Passed
+    @FindBy (id= "giftsLink")
     public static WebElement gifts;
-
     @FindBy(xpath ="//*[@id=\"link-rail\"]/div/div/div[1]/nav/ul/li[5]/a")
-    //@FindBy (id= "WEDDING REGISTRY")             // Passed
     public static WebElement weddingregistry;
-
-
-    //@FindBy (xpath ="//span[@id='myRewardsLabel-status']")
     @FindBy (xpath ="//*[@id='myRewardsLabel-status']/span[1]")
     public static WebElement signinmyaccount;
 
     public void checkLogo(){
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        logo.click();
-    }
+        logo.click();    }
     public void checkStores(){
        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        stores.click();
-   }
+        stores.click();  }
     public void checkDeals(){
        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        deals.click();
-    }
+        deals.click();   }
     public void checkLists(){
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        lists.click();
-    }
+        lists.click();   }
     public void checkGifts(){
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        gifts.click();
-    }
-    public void checkweddingregistry(){
+        gifts.click();   }
+    public void checkWeddingregistry(){
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        weddingregistry.click();
-    }
-    public void checksigninmyaccount(){
+        weddingregistry.click();   }
+    public void checkSigninmyaccount(){
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        signinmyaccount.click();
-    }
-}
+        signinmyaccount.click();   }
+        }
--- Macy's Module/src/test/java/testHomePage/TestMainPage.java
+++ Macy's Module/src/test/java/testHomePage/TestMainPage.java
@@ -1,10 +1,8 @@
 package testHomePage;
-
 import homePage.MainPage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import static reporting.TestLogger.log;
 
 public class TestMainPage extends MainPage {
@@ -14,48 +12,40 @@ public class TestMainPage extends MainPage {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
         mainPage = PageFactory.initElements(driver, MainPage.class);
-        setUrl("https://www.macys.com/");
-    }
-    @Test                                               // Test Passed
+        setUrl("https://www.macys.com/"); }
+    @Test
     public void testLogo() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkLogo();
-    }
+        mainPage.checkLogo();   }
     @Test
-    public void testStores() {                          //Test Passed
+    public void testStores() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkStores();
-    }
+        mainPage.checkStores(); }
     @Test
-    public void testDeals() {                            // Test Passed
+    public void testDeals() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkDeals();
-    }
+        mainPage.checkDeals();  }
     @Test
-    public void testLists() {                           // Test Passed
+    public void testLists() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkLists();
-    }
-    @Test                                               // Test Passed
+        mainPage.checkLists();  }
+    @Test
     public void testGifts() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkGifts();
-    }
-    @Test                                               // Test Passed
+        mainPage.checkGifts();  }
+    @Test
     public void testWeddingregistry() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checkweddingregistry();
-    }
-    @Test                                               // Test Passed
-    public void testsigninmyaccount() {
+        mainPage.checkWeddingregistry();  }
+    @Test
+    public void testSigninmyaccount() {
         log(getClass().getSimpleName() + ": " + convertToString(new Object() {
         }.getClass().getEnclosingMethod().getName()));
-        mainPage.checksigninmyaccount();
-    }
+        mainPage.checkSigninmyaccount();   }
    }
