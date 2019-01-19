22ba432e26d81f4969b9daa45c409f823ed4e052
OrfattChowdhury1 <orfatt11@gmail.com>
2019-01-18 23:11:58 -0800

    changes

--- UnitedHealthCare/src/main/java/dataProviderUtilityWithXlsReader/LogInByXls.java
+++ UnitedHealthCare/src/main/java/dataProviderUtilityWithXlsReader/LogInByXls.java
@@ -1,4 +1,37 @@
 package dataProviderUtilityWithXlsReader;
 
-public class LogInByXls {
+import base.BaseUtil;
+import homePage.SignInAndPopUpHandling;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.DataProvider;
+import reporting.TestLogger;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class LogInByXls extends BaseUtil {
+    @FindBy(xpath = "//a[text()='Login or Register']")
+    public static WebElement logInKey;
+    @FindBy(xpath = "//input[@name='loginName']")
+    public static WebElement emailAddressOrLogIn;
+    @FindBy(xpath = "//input[@name='password']")
+    public static WebElement passwordField;
+    @FindBy(xpath = "//input[@class='btn rounded w100 primary js_loginBtn']")
+    public static WebElement signInSubmitButton;
+
+    @DataProvider
+    public Iterator<Object[]> supplyDataExcel(){
+        // TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        ArrayList<Object[]> testDataExcel = XlsDataReaderUtil.getDataFromExcel();
+        return testDataExcel.iterator();
+    }
+    public void signInByDataProvider(String email, String password) throws Exception{
+        Thread.sleep(3000);
+        logInKey.click();
+        emailAddressOrLogIn.sendKeys(email);
+        passwordField.sendKeys(password);
+        signInSubmitButton.click();
+    }
+
 }
