1a2566fb2f230343d4eb5af62a817e4ebd63d181
OrfattChowdhury1 <orfatt11@gmail.com>
2019-01-18 23:45:51 -0800

    update

--- UnitedHealthCare/src/main/java/dataProviderUtilityWithXlsReader/LogInByXls.java
+++ UnitedHealthCare/src/main/java/dataProviderUtilityWithXlsReader/LogInByXls.java
@@ -12,15 +12,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class LogInByXls extends BaseUtil {
-    @FindBy(how = How.ID, using = "loginmenubutton")
+    @FindBy(how = How.XPATH, using = "//input[@id='hsid-username']")
     public static WebElement logInKey;
     @FindBy(how = How.XPATH, using = "//a[@onclick=\"publishPostPageData('exitLink',{'actions': {'linkName': 'Sign In to myuhc.com - https://www.myuhc.com/member/prewelcome.do'}});\"]")
     public static WebElement logInUHC;
-    @FindBy(how = How.ID, using = "hsid-username")
+    @FindBy(how = How.CSS, using = "#hsid-username")
     public static WebElement emailAddressOrLogIn;
-    @FindBy(xpath = "hsid-password")
+    @FindBy(how = How.CSS, using = "#hsid-password")
     public static WebElement passwordField;
-    @FindBy(how = How.ID, using = "hsid-submit")
+    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Sign in')]")
     public static WebElement signInSubmitButton;
 
     @DataProvider
