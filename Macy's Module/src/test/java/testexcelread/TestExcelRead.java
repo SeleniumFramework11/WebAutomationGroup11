f44e1a5376d1bcc8f3a53b32c703b4ed63035ba7
nayeem3814 <nayeemsisir156@gmail.com>
2019-01-24 18:35:25 -0500

    Macy commit

---
+++ Macy's Module/src/test/java/testexcelread/TestExcelRead.java
@@ -0,0 +1,25 @@
+package testexcelread;
+
+import base.BaseUtil;
+import com.relevantcodes.extentreports.ExtentReports;
+import excelread.ExcelRead2;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import reporting.ExtentManager;
+
+public class TestExcelRead extends BaseUtil {
+    ExcelRead2 excelReaderPage;
+    ExtentReports reports;
+    @BeforeMethod
+    public void setUp() {
+        this.excelReaderPage = new ExcelRead2();
+        reports = ExtentManager.getInstance();
+        setUrl("https://www.macys.com");
+
+    }
+    @Test(priority = 1)
+    public void itemsSearchFromExcelData() throws InterruptedException {
+        excelReaderPage.searchFromExcel();
+        excelReaderPage.navigateBack();
+    }
+}
